c4a1b45b4916f68b1cb1ba5923a4b1b85eda6e44
Gabriel Evaristo <gabriel.evaristo.2004@gmail.com>
2025-09-29 00:20:55 -0300

    feat: init `events` app

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -92,6 +92,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "config/static"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
