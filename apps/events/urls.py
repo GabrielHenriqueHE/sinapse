43e528cbd67e2cb3c1229e64da994751d5871f3d
Gabriel Evaristo <gabriel.evaristo.2004@gmail.com>
2025-10-01 22:21:12 -0300

    wip: update events templates

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -4,5 +4,6 @@ from apps.events import views
 
 urlpatterns = [
     path("", views.events, name="events_index"),
-    path("create", views.create_event, name="create_event")
+    path("create", views.create_event, name="create_event"),
+    path("<uuid:id>", views.event_details, name="event_details")
 ]
--- apps/events/views.py
+++ apps/events/views.py
@@ -6,6 +6,12 @@ def events(request):
     template = loader.get_template("events/index.html")
     return HttpResponse(template.render(None, request))
 
+
 def create_event(request):
     template = loader.get_template("events/create_event.html")
+    return HttpResponse(template.render(None, request))
+
+
+def event_details(request, id):
+    template = loader.get_template("events/event_details.html")
     return HttpResponse(template.render(None, request))
